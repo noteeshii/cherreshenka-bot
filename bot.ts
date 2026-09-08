984251c57e53d8ff6d721f4c8c349d464cc44ec8
noteeshii <noteeshii@gmail.com>
2026-09-08 22:22:28 +0500

    fix: redemption cancel

--- bot.ts
+++ bot.ts
@@ -88,7 +88,7 @@ export class Bot {
 
     await action.run(reward, { twitch: this.twitch, music: this.music, logger: this.logger });
 
-    await this.twitch.cancelRedemption(reward.redemptionId, reward.reward.id);
+    await this.twitch.cancelRedemption(reward.id, reward.reward.id);
 
     rewardLogger.debug(`Completed: ${reward.reward.title}`);
   }
