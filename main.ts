e1bf3fffa1350a7bc5a5890df4162cd2a5393284
noteeshii <noteeshii@gmail.com>
2026-09-11 01:14:33 +0500

    ref: some debug

--- main.ts
+++ main.ts
@@ -65,7 +65,5 @@ process.on('SIGINT', shutdown);
 process.on('SIGTERM', shutdown);
 
 await client.connect().catch(logger.error.bind(logger)).then(async() => {
-  const user = await twitch.getUser('noteeshii');
-
-  console.log(JSON.stringify(user, undefined, 2));
+  await twitch.timeout('zigimzigi');
 });
